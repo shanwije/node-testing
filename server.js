9363816734d8f34830b415334cf063821e0ff9fe
shanwije <shan.wijenayaka@ucdconnect.ie>
2018-04-13 23:07:33 +0530

    chat app working 100%

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+//---------------------------
 var port = 3000;
-
-app.get('/msg', (req, res)=>{
-    res.send('hello fucker');
-})
-
 app.use(express.static(__dirname));
 var server = app.listen(port, ()=>{
     console.log('server listning to',server.address());
 });
+//----------------------------
+
+var msg = [];
+
+app.post('/msg', (req, res)=>{
+    console.log("request is ",req.body);
+    msg.push(req.body);
+    res.send(msg);
+    // res.sendStatus(200);
+})
+
