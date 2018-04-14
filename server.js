b41d1a4ab8d76d6858c6df5ff68cad30bde9beff
shanwije <shan.wijenayaka@ucdconnect.ie>
2018-04-14 13:18:34 +0530

    socket io added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 
-var app = express();
+var app = express();  
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -12,16 +14,21 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 //---------------------------
 var port = 3000;
 app.use(express.static(__dirname));
-var server = app.listen(port, ()=>{
+var server = http.listen(port, ()=>{
     console.log('server listning to',server.address());
 });
 //----------------------------
+
+io.on('connection', (socket) =>{
+    console.log('user connected');
+})
 
 var msg = [];
 
 app.post('/msg', (req, res)=>{
     console.log("request is ",req.body);
     msg.push(req.body);
+    io.emit('myMsg',JSON.stringify(msg));
     res.send(msg);
     // res.sendStatus(200);
 })
